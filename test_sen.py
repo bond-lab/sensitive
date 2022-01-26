c33319ce9a2fcb4f15c5193fa587fe2ec18d9fe9
Francis Bond <bond@ieee.org>
2022-01-26 16:38:09 +0800

    added some more simple tests for boosters and negators

--- test_sen.py
+++ test_sen.py
@@ -1,20 +1,31 @@
 import sensitive
 from sensitive.sensitive import SentimentAnalyzer
 
-sentences = ["VADER is smart, handsome, and funny.",
+sentences = [#"VADER is smart, handsome, and funny.",
              "VADER is happy.",
-             "VADER is happy!",
-             "VADER is happy!!!",
-             "VADER is happy?",
-             "VADER is happy???",
-             "VADER is not happy.",
-             "SENSI is more happy.",
-             "SENSI is happier.",
-             "SENSI is very happy.",
-             "This is unhappy.",
-             "Wordnet is happiest.",
-             "The happy wordnet has some problems.",
-             "The happy wordnet has some PROBLEMS."]
+     "VADER is very happy.",
+    "VADER is slightly happy.",
+    "VADER is very slightly happy.",
+    "VADER is very very happy.",
+    "VADER is very very very happy.",
+     "VADER is not happy.",
+     "VADER is not very happy.",
+    "VADER is very not happy.",
+    # "VADER is happy!",
+    # "VADER is happy!!!",
+    # "VADER is happy?",
+    #          "VADER is happy???",
+    #          "VADER is not happy.",
+    #          "SENSI is more happy.",
+    #          "SENSI is happier.",
+    #          "SENSI is very happy.",
+    #          "This is unhappy.",
+    #          "Wordnet is happiest.",
+    "The happy wordnet has some problems.",
+    "The happy wordnet has no problems.",
+    "The happy wordnet has some PROBLEMS.",
+    "The happy wordnet has NO PROBLEMS."
+]
 
 analyzer = SentimentAnalyzer()
 
